ddd6a1c87208eba401cbd05f3725ab6c10b34705
ldrohan <drohanluke@gmail.com>
2015-03-24 12:49:20 -0700

    g notes controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  resources :notes
+
 end
+
+
+# Prefix Verb   URI Pattern               Controller#Action
+# welcome_index GET    /welcome/index(.:format)  welcome#index
+#          root GET    /                         welcome#index
+#         notes GET    /notes(.:format)          notes#index
+#               POST   /notes(.:format)          notes#create
+#      new_note GET    /notes/new(.:format)      notes#new
+#     edit_note GET    /notes/:id/edit(.:format) notes#edit
+#          note GET    /notes/:id(.:format)      notes#show
+#               PATCH  /notes/:id(.:format)      notes#update
+#               PUT    /notes/:id(.:format)      notes#update
+#               DELETE /notes/:id(.:format)      notes#destroy
